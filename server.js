c7b247715bd9d009f1ed323c5464a4c3022e04fd
Gustavo Esporte dos Santos <75767594+EsporteG@users.noreply.github.com>
2026-06-15 13:48:11 -0300

    Update server.js

--- server.js
+++ server.js
@@ -10,6 +10,7 @@ const path = require("path");
 const app = express();
 app.use(express.json());
 app.use(cors());
+app.use(express.static(path.join(__dirname, "public")));
 
 // ─── Configuração Twilio ───────────────────────────────────────────────────
 const twilioClient = twilio(
